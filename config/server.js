f8499829950e53ffee06b3800231acdc9d11fed3
Luis Bejarano <laloeduar208@gmail.com>
2022-05-19 18:02:23 -0500

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '12c93b400f972a400db2b91504eae303'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c3d3ab28-63e6-4fa9-ab5b-8eb1d82382b0'
+};
